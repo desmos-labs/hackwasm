8677ed1cf2816a3ff571fb749fec9f6e63dec32c
dadamu <p22626262@gmail.com>
2022-10-01 15:50:31 -0500

    feat: implemented send ibc msg method

--- contracts/post-cyber-index/src/msg.rs
+++ contracts/post-cyber-index/src/msg.rs
@@ -1,12 +1,12 @@
+use cosmwasm_std::{Addr, Uint64};
+use cyber_std::Link;
 use schemars::JsonSchema;
 use serde::{Deserialize, Serialize};
-use cosmwasm_std::{Uint64, Addr};
 
 #[derive(Serialize, Deserialize, Clone, Debug, PartialEq, JsonSchema)]
 pub struct InstantiateMsg {
     pub subspace_id: Uint64,
     pub cyber_contract_address: Addr,
-    pub channel_id: String,
     pub root_hash: String,
 }
 
@@ -19,3 +19,8 @@ pub enum ExecuteMsg {
 #[derive(Serialize, Deserialize, Clone, Debug, PartialEq, JsonSchema)]
 #[serde(rename_all = "snake_case")]
 pub enum QueryMsg {}
+
+#[derive(Serialize, Deserialize, Clone, PartialEq, JsonSchema, Debug)]
+pub struct Packet {
+    pub links: Vec<Link>,
+}
--- contracts/post-cyber-index/src/state.rs
+++ contracts/post-cyber-index/src/state.rs
@@ -1,14 +1,21 @@
+use cosmwasm_schema::cw_serde;
+use cosmwasm_std::{Addr, IbcEndpoint};
+use cw_storage_plus::Item;
 use schemars::JsonSchema;
 use serde::{Deserialize, Serialize};
-use cosmwasm_std::Addr;
-use cw_storage_plus::Item;
 
 #[derive(Serialize, Deserialize, Clone, Debug, PartialEq, JsonSchema)]
 pub struct State {
     pub subspace_id: u64,
     pub cyber_contract_address: Addr,
-    pub channel_id: String,
     pub root_hash: String,
 }
 
 pub const STATE: Item<State> = Item::new("state");
+
+#[cw_serde]
+pub struct ChannelInfo {
+    pub id: String,
+    pub counterparty_endpoint: IbcEndpoint,
+    pub connection_id: String,
+}

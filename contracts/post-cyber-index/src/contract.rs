0334b6fe89320647e9075663829a428f7719c0d6
dadamu <p22626262@gmail.com>
2022-10-01 18:08:07 -0500

    chore: replace instantiate msg into empty

--- contracts/post-cyber-index/src/contract.rs
+++ contracts/post-cyber-index/src/contract.rs
@@ -1,7 +1,7 @@
 #[cfg(not(feature = "library"))]
 use cosmwasm_std::entry_point;
 use cosmwasm_std::{
-    to_binary, Binary, Deps, DepsMut, Env, IbcMsg, MessageInfo, Response, Timestamp,
+    to_binary, Binary, Deps, DepsMut, Env, IbcMsg, MessageInfo, Response, Timestamp, Empty
 };
 use cw2::set_contract_version;
 use desmos_bindings::posts::querier::PostsQuerier;
@@ -21,7 +21,7 @@ pub fn instantiate(
     deps: DepsMut,
     _env: Env,
     info: MessageInfo,
-    _msg: InstantiateMsg,
+    _msg: Empty,
 ) -> Result<Response, ContractError> {
     set_contract_version(deps.storage, CONTRACT_NAME, CONTRACT_VERSION)?;
     Ok(Response::new()
@@ -78,7 +78,3 @@ fn get_cyber_links_from_post(
     }])
 }
 
-#[cfg_attr(not(feature = "library"), entry_point)]
-pub fn query(_deps: Deps, _env: Env, _msg: QueryMsg) -> Result<Binary, ContractError> {
-    unimplemented!()
-}

b910b5cb819413839831eca1f66041b5e46348a7
dadamu <p22626262@gmail.com>
2022-10-02 11:12:47 -0500

    feat: added particle functions

--- contracts/post-cyber-index/src/lib.rs
+++ contracts/post-cyber-index/src/lib.rs
@@ -3,5 +3,6 @@ mod error;
 pub mod ibc;
 pub mod msg;
 pub mod state;
+pub mod utils;
 
 pub use crate::error::ContractError;
---
+++ contracts/post-cyber-index/src/utils.rs
@@ -0,0 +1,39 @@
+/// Copy from https://github.com/cybercongress/cw-cyber/packages/cyber-std/src/particle.rs
+use std::ops::Add;
+use std::str::FromStr;
+use cid::{Cid, Version};
+use cid::multihash::{Code, MultihashDigest};
+use crate::error::ContractError;
+
+pub fn prepare_particle(input: String) -> Result<Cid, ContractError> {
+    if input.len() == 0 || input.len() > 256 {
+        return Err(ContractError::InvalidParticleData {});
+    }
+
+    // unixfs/dagnode/proto shortcut
+    // wrap input bytes as a dagnode unixfs file
+    let length: u8 = input.len() as u8;
+    let mut raw: Vec<u8> = vec![10, length.add(6) as u8, 8, 2, 18, length];
+    raw.append(&mut input.as_bytes().to_vec());
+    raw.append(&mut vec![24, length]);
+
+    let h = Code::Sha2_256.digest(&raw.as_slice());
+    let particle = Cid::new_v0(h).unwrap();
+
+    Ok(particle)
+}
+
+pub fn check_particle(input: String) -> Result<Cid, ContractError> {
+    let particle:Cid;
+    let try_particle = Cid::from_str(&input.clone());
+    if try_particle.is_ok() {
+        particle = try_particle.unwrap();
+        if particle.version() != Version::V0 {
+            return Err(ContractError::InvalidParticleVersion {});
+        }
+    } else {
+        return Err(ContractError::InvalidParticle {});
+    }
+
+    Ok(particle)
+}
